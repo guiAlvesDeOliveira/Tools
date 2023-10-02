69327da94b75ca3bfd4e31b9c1e02e93a04853e2
J34993R <guialves.oliveira1234@gmail.com>
2023-10-02 14:03:09 -0300

    test: Add new tests for CepValidator

--- src/test/java/com/jtools/jtools/cepvalidation/CepValidatorTest.java
+++ src/test/java/com/jtools/jtools/cepvalidation/CepValidatorTest.java
@@ -1,6 +1,8 @@
 package com.jtools.jtools.cepvalidation;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 
@@ -9,8 +11,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class CepValidatorTest {
 
     @Test
-    void validCepShouldReturnTrue() throws IOException {
+    void validCepWithoutDashShouldReturnTrue() throws IOException {
+        CepValidator cepValidator = new CepValidator();
+        assertTrue(cepValidator.isCepValid("01001000"));
+    }
+    @Test
+    void validCepWithDashShouldReturnTrue() throws IOException {
         CepValidator cepValidator = new CepValidator();
         assertTrue(cepValidator.isCepValid("01001-000"));
+    }
+
+    @Test
+    void validCepWithoutDashShouldReturnFalse() throws IOException {
+        CepValidator cepValidator = new CepValidator();
+        assertFalse(cepValidator.isCepValid("12345678"));
+    }
+    @Test
+    void validCepWithDashShouldReturnFalse() throws IOException {
+        CepValidator cepValidator = new CepValidator();
+        assertFalse(cepValidator.isCepValid("12345-678"));
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void invalidCepShouldReturnApiRequestException(String cep) {
+        CepValidator cepValidator = new CepValidator();
+        assertThrows(CepValidator.ApiRequestException.class, () -> cepValidator.isCepValid(cep));
     }
 }
